ec1cb48a6263a7bb0ff2229b4216fc8af17d509f
Diana <d.dyulberova@gmail.com>
2022-09-01 18:49:32 +0500

    Example 005 SumRandom project

---
+++ Example005_SumRandom/Program.cs
@@ -0,0 +1,7 @@
+﻿// Создаем программу сложения двух случайных чисел, как пример 5 из первой лекции.
+int numberA = new Random().Next(1,10); // 1,2,3..9
+Console.WriteLine (numberA);
+int numberB = new Random().Next(1,10); // 1,2,3..9
+Console.WriteLine (numberB);
+int result = numberA + numberB;
+Console.WriteLine (result);
