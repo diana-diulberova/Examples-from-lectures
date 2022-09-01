c7b084c4dca7441f2035e9af12aa738ee815509c
Diana <d.dyulberova@gmail.com>
2022-09-01 17:29:38 +0500

    Example 002 Hello User project

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿// Создаем программу приветствия "Hello, User" как пример 2 из первой лекции.
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
