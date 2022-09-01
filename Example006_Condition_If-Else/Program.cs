7e99d1e850b8b319916dee918a6711f56b91911f
Diana <d.dyulberova@gmail.com>
2022-09-01 20:34:27 +0500

    Example 006 Conditio If-Else project

---
+++ Example006_Condition_If-Else/Program.cs
@@ -0,0 +1,12 @@
+﻿// Создаем программу приветствия для пользователя-любимчика как пример 6 из первой лекции.
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же МАША!");
+}
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine("username");
+}
