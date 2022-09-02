a1ef65af15288ccf5dcba63d0f4815573eeaf0e5
Diana <d.dyulberova@gmail.com>
2022-09-02 13:51:39 +0500

    Example 007 Base project

---
+++ Example007_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿// Создаем программу для нахождения веса самой тяжелой гири как пример 7 из первой лекции.
+int a = 2;
+int b = 4;
+int c = 6;
+int d = 8;
+int e = 5;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
