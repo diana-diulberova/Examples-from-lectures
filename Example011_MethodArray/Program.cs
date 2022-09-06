7669c0146d7d272f736db177f31df1dea01aeb1c
Diana <d.dyulberova@gmail.com>
2022-09-06 13:10:59 +0500

    Ищем индекс числа в массиве

---
+++ Example011_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {1, 22, 43, 5, 94, 43, 32, 45, 2};
+
+int n = array.Length;
+int find = 43;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // останавливает цикл на первом найденном варианте (если в массиве несколько одинаковых значений)
+    }
+    index++; // index = index + 1
+    }
